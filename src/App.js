ce9dc9fc4826e4f96e2ab93fc946e93f6505c0f5
yeji Sung <dpwl0332@naver.com>
2024-02-01 13:02:38 +0900

    context branch 생성

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <>branch props-drilling</>;
+  return (
+    <>
+      branch props-drilling <br />
+      branch context
+    </>
+  );
 }
 
 export default App;
